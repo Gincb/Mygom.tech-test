ef3469cccebefc32fa07569ef62b5ef57af658cf
Gintare B <gintaar3@gmail.com>
2021-09-27 13:21:06 +0300

    logout

--- src/components/UsersManagement/components/Header/Header.tsx
+++ src/components/UsersManagement/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import {FC} from 'react';
+import { useHistory } from 'react-router';
 import {IItem} from "~/services/getUserItems";
 import logout from '../../../../services/logout';
+import { Routes } from "../../../../constants";
 
 import './header-style.scss';
 
@@ -10,11 +12,17 @@ interface IHeader {
 }
 
 const Header: FC<IHeader> = ({items, username}) => {
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    await logout();
+    history.push(Routes.Login);
+  }
 
   return (
     <div className="header">
       <div className="user-section">
-        <button onClick={logout}>{`Logout ${username}`}</button>
+        <button onClick={handleLogout}>{`Logout ${username}`}</button>
       </div>
       <h1>{`${items.length} Emails are wrong`}</h1>
       <span>Email validator to protect your company from bad registrations</span>
--- src/services/logout.ts
+++ src/services/logout.ts
@@ -1,5 +1,10 @@
-const logout = () => {
+import {API} from '~/constants';
+import getUrl from '../utils/getUrl';
 
+const logout = async() => {
+  localStorage.removeItem("token");
+  const url = getUrl(API.Logout );
+  await fetch(url);
 };
 
 export default logout;
