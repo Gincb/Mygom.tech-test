a9889d801f0af20fdc7ac8e2c2fc4686fc2b06f0
Gintare B <gintaar3@gmail.com>
2021-09-27 15:15:23 +0300

    validation errors

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -11,25 +11,60 @@ const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState<string>();
+  const [usernameError, setUsernameError] = useState<string>();
+  const [passwordError, setPasswordError] = useState<string>();
+
+  const usernameValidation = () => {
+    if(username.length == 0) {
+      setUsernameError('Please enter your username');
+    } else if(username.length <= 4) {
+      setUsernameError('Your username should be longer than 4 letters');
+    } else if(checkSymbols(username)) {
+      setUsernameError('Your username shouldn`t contain other symbols than _ or -')
+    } else {
+      setUsernameError('')
+    }
+
+    return usernameError;
+  }
+
+  const checkSymbols = (username: string) => {
+    const symbols = /[ `!@#$%^&*()+\=\[\]{};':"\\|,.<>\/?~]/;
+    const checkForSymbols = symbols.test(username);
+    return checkForSymbols;
+  }
+
+  const passwordVaildation = () => {
+    if(password.length == 0) {
+      setPasswordError('Please enter your password');
+    } else if(password.length <= 4) {
+      setPasswordError('Your password should be longer than 4 letters');
+    } else {
+      setPasswordError('')
+    }
+  }
 
   const handleSubmit = async (event: SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
     setErrorMessage(null);
+    usernameValidation();
+    passwordVaildation();
 
     try {
       await login(username, password);
       push(Routes.Users);
     } catch (error) {
-      setErrorMessage(error.message);
+      setErrorMessage('Password is incorrect or a user with that name doesn`t exist');
     }
   };
 
-  return (
+    return (
     <div className="login-page">
       <form className="login-form" onSubmit={handleSubmit}>
         <h1 className="text-center">
           Mygom.tech
         </h1>
+
         <input
           value={username}
           onChange={(event) => setUsername(event.target.value)}
@@ -37,6 +72,7 @@ const Login = () => {
           type="text"
           className="input mt-52px"
         />
+        <ErrorBlock error={usernameError}/>
         <input
           value={password}
           onChange={(event) => setPassword(event.target.value)}
@@ -44,7 +80,8 @@ const Login = () => {
           type="password"
           className="input mt-24px"
         />
-        <ErrorBlock error={errorMessage}/>
+        <ErrorBlock error={passwordError}/>
+        {usernameError || passwordError ? '' : <ErrorBlock error={errorMessage}/>}
         <button type="submit" className="button mt-24px">
           Login
         </button>
